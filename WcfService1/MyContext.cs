40849986d186e903cf7da4a75d7ebab90582f092
novakpatrik <novakpatrik@L108PC15>
2017-03-31 10:39:52 +0200

    test

---
+++ WcfService1/MyContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WcfService1
+{
+    public class MyContext : DbContext
+    {
+    }
+}

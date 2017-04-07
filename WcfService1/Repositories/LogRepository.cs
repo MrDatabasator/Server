1af6b2412623b00ff591838119f7a9bb341227bc
novakpatrik <novakpatrik@L108PC15>
2017-04-07 10:37:38 +0200

    Repositories

---
+++ WcfService1/Repositories/LogRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfService1
+{
+    public class LogRepository
+    {
+        private MyContext _context = new MyContext();
+    }
+}
---
+++ WcfService1/Repositories/TaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfService1
+{
+    public class TaskRepository
+    {
+        private MyContext _context = new MyContext();
+    }
+}
